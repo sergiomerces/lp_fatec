c67920d7b5a4a9a7b34c4b671699519236f3000c
sergiomerces <sergiomerces@outlook.com>
2019-10-23 21:59:06 -0300

    'Atualizacao Jogo de Dados1'

---
+++ csharp/jogo_dados1/jogo_dados1/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace jogo_dados1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Random NumeroRandomico = new Random(); 
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DisplayDado(pictureBox1);
+        }
+
+        public void DisplayDado(PictureBox Lado) 
+        { 
+            int face = NumeroRandomico.Next(1, 7); 
+            Lado.Image = Image.FromFile("C:\\Users\\sergi\\OneDrive\\Documentos\\Fatec-DESKTOP-OHV2I8K\\programacao\\imagens\\imagens\\die" + face + ".gif"); 
+        }
+    }
+}
