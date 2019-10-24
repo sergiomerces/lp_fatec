7fbb0c7c9953388ab8d3dc34945b2e8bccb8ff99
sergiomerces <sergiomerces@outlook.com>
2019-10-23 22:21:39 -0300

    Atualizacao

---
+++ csharp/jogo_dados2/jogo_dados2/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace jogo_dados2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Random randomNumber = new Random(); 
+
+        private void btnRoll_Click(object sender, EventArgs e)
+        {
+            DisplayDie(dieLabel1); DisplayDie(dieLabel2); DisplayDie(dieLabel3); DisplayDie(dieLabel4);
+
+        }
+    }
+}
